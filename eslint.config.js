b1907bb327403dce96d269bc7eb3294e2ac63063
Ayodev <aayo20dev@gmail.com>
2024-09-13 21:17:39 +0100

    More Changes

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+module.export = [
+    {
+      files: ["**/*.js"], // Adjust according to your project file types
+      rules: {
+        semi: "error",
+        "no-unused-vars": "warn",
+        "no-console": "off"
+      },
+    },
+  ];
+  
